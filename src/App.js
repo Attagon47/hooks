ad3a63d9aa00e016156db533b578ca9340982cb6
Attagon47 <attagon.bo.65@ubu.ac.th>
2023-07-11 12:02:46 +0700

    add message

--- src/App.js
+++ src/App.js
@@ -1,23 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [state, setState] = useState(0);
+  const [totalPrice, setTotalPrice] = useState(0);
+  const [totalItems, setTotalItems] = useState(0);
+  const [product1, setProduct1] = useState(39990);
+  const [product2, setProduct2] = useState(34990);
+  const [product3, setProduct3] = useState(33990);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h3>จำนวนสินค้าในตะกร้า {totalItems}</h3>
+        <h3>ราคาสินค้าในตะกร้าทั้งหมด {totalPrice}</h3>
+        <img src="01.jpg" alt="case" width="15%" />
+      </div>
+      <div>
+        <br />
+        Asus TUF Gaming F17 FX707ZC-HX035W ราคา {product1} บาท
+      </div>
+      <button
+        onClick={() => {
+          setTotalItems(totalItems + 1);
+          setTotalPrice(totalPrice + product1);
+        }}
+      >
+        เพิ่มเข้าตะกร้า
+      </button>
+      <button
+        onClick={() => {
+          if (totalItems > 0) {
+            setTotalItems(totalItems - 1);
+            setTotalPrice(totalPrice - product1);
+          }
+        }}
+      >
+        นำออกจากตะกร้า
+      </button>
+      <div>
+        <img src="02.jpg" alt="case" width="15%" />
+        <br />
+        Asus ROG Strix G15 G513RC-LP179W ราคา {product2} บาท
+      </div>
+      <button
+        onClick={() => {
+          setTotalItems(totalItems + 1);
+          setTotalPrice(totalPrice + product2);
+        }}
+      >
+        เพิ่มเข้าตะกร้า
+      </button>
+      <button
+        onClick={() => {
+          if (totalItems > 0) {
+            setTotalItems(totalItems - 1);
+            setTotalPrice(totalPrice - product2);
+          }
+        }}
+      >
+        นำออกจากตะกร้า
+      </button>
+      <div>
+        <img src="03.jpg" alt="case" width="15%" />
+        <br />
+        Asus TUF Gaming F15 FX506HM-HN130W ราคา {product3} บาท
+      </div>
+      <button
+        onClick={() => {
+          setTotalItems(totalItems + 1);
+          setTotalPrice(totalPrice + product3);
+        }}
+      >
+        เพิ่มเข้าตะกร้า
+      </button>
+      <button
+        onClick={() => {
+          if (totalItems > 0) {
+            setTotalItems(totalItems - 1);
+            setTotalPrice(totalPrice - product3);
+          }
+        }}
+      >
+        นำออกจากตะกร้า
+      </button>
     </div>
   );
 }
